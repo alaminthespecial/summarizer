2be6fa66e64fb5a71bed53abc2380b8d18ecbe92
alaminthespecial <alaminthespecial@gmail.com>
2024-01-28 06:34:32 -0500

    This is a new commit for what I originally planned to be amended

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-}
+    exportPathMap: {
+        "/": { page: "/" }
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
